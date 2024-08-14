4d53695bd569c8c9662b14d2e8808b679309d05e
unknown <rewoly@mail.ru>
2024-08-14 17:28:17 +0900

    Работа с клиентской формой

--- gatekeeper_client_single/src/main/java/com/alrosa/staa/gatekeeper_client_single/controller/MainPageController.java
+++ gatekeeper_client_single/src/main/java/com/alrosa/staa/gatekeeper_client_single/controller/MainPageController.java
@@ -1,5 +1,17 @@
 package com.alrosa.staa.gatekeeper_client_single.controller;
 
-public class MainPageController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
+public class MainPageController {
+    @FXML
+    private Button buttonUpdateUser = new Button();
+    @FXML
+    private Button buttonEditUser = new Button();
+    @FXML
+    private Button buttonAddUser = new Button();
+    @FXML
+    private Button buttonDeleteUser = new Button();
+    @FXML
+    private Button buttonOpenLogs = new Button();
 }
