0893ac894d5a09784eb704cc4a7d23dcfcb00ba3
unknown <rewoly@mail.ru>
2024-08-14 12:01:32 +0900

    работа с БД

--- gatekeeper_server_single/src/main/java/com/alrosa/staa/gatekeeper_server_single/controller/ApplicationController.java
+++ gatekeeper_server_single/src/main/java/com/alrosa/staa/gatekeeper_server_single/controller/ApplicationController.java
@@ -1,11 +1,28 @@
 package com.alrosa.staa.gatekeeper_server_single.controller;
 
+import com.alrosa.staa.gatekeeper_server_single.entity.UserEntity;
+import com.alrosa.staa.gatekeeper_server_single.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @RestController
 public class ApplicationController {
+    @Autowired
+    private UserService userService;
     @GetMapping("/")
     private String getInfo() {
         return "Система контроля и управления доступом. Версия для работы с одним контроллером";
     }
+    @GetMapping("/getUsers")
+    private List<UserEntity> getUsers() {
+        return userService.readUsers();
+    }
+    @GetMapping("/getUser/{id}")
+    private UserEntity getUser(@PathVariable String id) {
+        return userService.readUser(Long.parseLong(id));
+    } 
 }
