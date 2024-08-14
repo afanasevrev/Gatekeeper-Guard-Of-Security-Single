7d6192ce28c46174a6918e9e70b5e25f54c214cd
unknown <rewoly@mail.ru>
2024-08-14 11:52:02 +0900

    работа с БД

--- gatekeeper_server_single/src/main/java/com/alrosa/staa/gatekeeper_server_single/GatekeeperServerSingleApplication.java
+++ gatekeeper_server_single/src/main/java/com/alrosa/staa/gatekeeper_server_single/GatekeeperServerSingleApplication.java
@@ -1,7 +1,14 @@
 package com.alrosa.staa.gatekeeper_server_single;
 
+import com.alrosa.staa.gatekeeper_server_single.RabbitMQ.RabbitConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+@Import(RabbitConfiguration.class)
+@EnableAutoConfiguration
+@ComponentScan
 @SpringBootApplication
 public class GatekeeperServerSingleApplication {
 	public static void main(String[] args) {
---
+++ gatekeeper_server_single/src/main/java/com/alrosa/staa/gatekeeper_server_single/RabbitMQ/RabbitConfiguration.java
@@ -0,0 +1,4 @@
+package com.alrosa.staa.gatekeeper_server_single.RabbitMQ;
+
+public class RabbitConfiguration {
+}
