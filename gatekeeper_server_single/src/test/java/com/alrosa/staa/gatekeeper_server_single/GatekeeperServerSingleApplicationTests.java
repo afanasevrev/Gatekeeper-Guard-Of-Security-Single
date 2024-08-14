403ae201dc90476600f4da058aef749d5961d08d
unknown <rewoly@mail.ru>
2024-08-14 09:55:23 +0900

    first commit

---
+++ gatekeeper_server_single/src/test/java/com/alrosa/staa/gatekeeper_server_single/GatekeeperServerSingleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alrosa.staa.gatekeeper_server_single;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatekeeperServerSingleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
