810f74bb5f741778328c153e658d4efd5773f048
unknown <rewoly@mail.ru>
2024-08-14 10:08:40 +0900

    Добавил драйвер perco

---
+++ gatekeeper_perco_driver_single/src/main/java/com/alrosa/staa/gatekeeper_perco_driver_single/GatekeeperPercoDriverSingleApplication.java
@@ -0,0 +1,13 @@
+package com.alrosa.staa.gatekeeper_perco_driver_single;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatekeeperPercoDriverSingleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatekeeperPercoDriverSingleApplication.class, args);
+	}
+
+}
---
+++ gatekeeper_perco_driver_single/src/test/java/com/alrosa/staa/gatekeeper_perco_driver_single/GatekeeperPercoDriverSingleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alrosa.staa.gatekeeper_perco_driver_single;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatekeeperPercoDriverSingleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
