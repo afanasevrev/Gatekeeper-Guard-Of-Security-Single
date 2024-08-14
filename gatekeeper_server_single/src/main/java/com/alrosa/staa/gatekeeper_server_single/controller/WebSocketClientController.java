264f206d082707499f83c56f5e75c5977fb8d115
unknown <rewoly@mail.ru>
2024-08-14 12:19:41 +0900

    работа с БД

--- gatekeeper_server_single/src/main/java/com/alrosa/staa/gatekeeper_server_single/controller/WebSocketClientController.java
+++ gatekeeper_server_single/src/main/java/com/alrosa/staa/gatekeeper_server_single/controller/WebSocketClientController.java
@@ -1,7 +1,10 @@
 package com.alrosa.staa.gatekeeper_server_single.controller;
 
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebSocketClientController {
-
+    @Autowired
+    private AmqpTemplate amqpTemplate;
 }
