8389b83fed97512fed84d5bafe540e40c3ceeed1
afanasevrev <rewoly@mail.ru>
2024-08-14 22:34:53 +0900

    Работа с клиентской частью

--- gatekeeper_client_single/src/main/java/com/alrosa/staa/gatekeeper_client_single/controller/MainPageController.java
+++ gatekeeper_client_single/src/main/java/com/alrosa/staa/gatekeeper_client_single/controller/MainPageController.java
@@ -4,10 +4,14 @@ import com.alrosa.staa.gatekeeper_client_single.data.UserData;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-public class MainPageController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainPageController implements Initializable {
     @FXML
     private Button buttonUpdateUser = new Button();
     @FXML
@@ -20,10 +24,15 @@ public class MainPageController {
     private Button buttonOpenLogs = new Button();
     @FXML
     private TableView<UserData> tableViewUsers = new TableView<UserData>();
-    private ObservableList<UserData> observableListUserData = FXCollections.<UserData>observableArrayList();
+    private final ObservableList<UserData> observableListUserData = FXCollections.<UserData>observableArrayList();
     @FXML
     private TableColumn<UserData, String> tableColumnUserId = new TableColumn<UserData, String>("ID");
     @FXML
     private TableColumn<UserData, String> tableColumnUserName = new TableColumn<UserData, String>("Полное имя");
-
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        tableViewUsers.setItems(observableListUserData);
+        tableColumnUserId.setCellValueFactory(cellData -> cellData.getValue().idProperty());
+        tableColumnUserName.setCellValueFactory(cellData -> cellData.getValue().fullNameProperty());
+    }
 }
