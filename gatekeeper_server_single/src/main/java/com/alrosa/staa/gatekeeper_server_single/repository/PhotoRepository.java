213571a132561ec3ae46e91ae7713505eab83faa
afanasevrev <rewoly@mail.ru>
2024-09-22 23:30:40 +0900

    Прокомментировал репозиторий

--- gatekeeper_server_single/src/main/java/com/alrosa/staa/gatekeeper_server_single/repository/PhotoRepository.java
+++ gatekeeper_server_single/src/main/java/com/alrosa/staa/gatekeeper_server_single/repository/PhotoRepository.java
@@ -3,6 +3,9 @@ package com.alrosa.staa.gatekeeper_server_single.repository;
 import com.alrosa.staa.gatekeeper_server_single.entity.PhotoEntity;
 import com.alrosa.staa.gatekeeper_server_single.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+/**
+ * Репозиторий для хранения фотографий
+ */
 public interface PhotoRepository extends JpaRepository<PhotoEntity, Long> {
     PhotoEntity findByUserEntity(UserEntity userEntity);
 }
